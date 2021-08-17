e267f721ca239dbb49206a9b89655e970fffce35
lorincz.mate@gmail.com <LorinczMate1990@github.com>
2021-08-17 15:58:51 +0200

    Renaming things

--- index.js
+++ index.js
@@ -5,23 +5,24 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-// Where we will keep books
-let books = [];
+// Database interface
+let tickets = [];
+let people = [];
 
 app.use(cors());
 
 // Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // For testing: curl -d '{"bookName":333}' -H "Content-Type: application/json" -X POST http://localhost:3000/book
-app.post('/book', (req, res) => {
-    const book = req.body;
+app.post('/ticket', (req, res) => {
+    const ticket = req.body;
     // Output the book to the console for debugging
-    console.log(`Received body: `, book);
-    books.push(book);
-    console.log("Books: ", books)
-    res.send('Book is added to the database');
+    console.log(`Received body: `, ticket);
+    tickets.push(ticket);
+    console.log("Tickets: ", tickets)
+    res.send({"status": "OK"});
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
