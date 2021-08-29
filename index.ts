05a75389345c25e9d7bc5c28859c0b7163748373
lorincz.mate@gmail.com <LorinczMate1990@github.com>
2021-08-30 01:45:52 +0200

    Creating a package and a publisher pipeline

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-import {Record, RestCollection, RestApi} from "./modules/restapi"
+export {
+    Record, 
+    RestCollection, 
+    RestApi,
+} from "./modules/restapi"
 
-// Database interface
-
-/*
-let restApi = new RestApi(3000, [new VolatileRestCollection("/example")])
-restApi.registerEndpoint(new VolatileRestCollection("/example"))
-restApi.listen()*/
+export {
+    VolatileRestCollection
+} from "./modules/volatile-rest-collection"
--- unittest.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-tsc --esModuleInterop -t es5 tests/unit/unittest.ts && npx mocha tests/unit/unittest.js
